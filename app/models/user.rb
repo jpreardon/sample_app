b0af8da2fba0f2c00e620f7111a58357a3d3d3e3
John P. Reardon <jp@jpreardon.com>
2019-04-16 02:00:46 +0000

    Add password to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[^.]\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+                    
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new(name: 'Pat Castelli', email: 'pcastelli@alumni.westin.com')
+    @user = User.new(name: 'Pat Castelli', 
+                      email: 'pcastelli@alumni.westin.com',
+                      password: 'junkmo',
+                      password_confirmation: 'junkmo')
   end
   
   test 'should be valid' do
@@ -58,6 +61,16 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+  
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
   
 end
