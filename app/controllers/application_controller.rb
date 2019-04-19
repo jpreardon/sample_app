766bd9ba17bf32d54dc92a9cfa8404e36bc95c2e
John P. Reardon <jp@jpreardon.com>
2019-04-19 14:35:47 +0000

    Remove old hello method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
-  # TODO: Remove this crap
-  def hello
-    render html: "hi"
-  end
 end
