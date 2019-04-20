e7d56e04b328fba95e734b253df183a9fe569618
John P. Reardon <jp@jpreardon.com>
2019-04-20 16:26:37 +0000

    Add delete user function and tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,4 +54,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                             admin: true } }
     assert_not @otheruser.reload.admin?
   end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@otheruser)
+    assert_no_difference 'User.count', 'You have problems here, non-admins can delete users.' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
