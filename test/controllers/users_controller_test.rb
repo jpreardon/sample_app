e44ae6d5773f5f45e316eae3cc18845e01420815
John P. Reardon <jp@jpreardon.com>
2019-04-20 02:43:53 +0000

    Add authorization

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @user = users(:keith)
+    @otheruser = users(:mick)
   end
   
   test "should get new" do
@@ -22,6 +23,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                           email: @user. email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+  
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@otheruser, password: '123456')
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@otheruser, password: '123456')
+    patch user_path(@user), params: { user: { name: @user.name,
+                                          email: @user. email } }
+    assert_not flash.empty?
+    assert_redirected_to root_url
   end
 
 end
