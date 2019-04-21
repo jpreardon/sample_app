51a7bcae9242c55439ea0eae209cca78f184002b
John P. Reardon <jp@jpreardon.com>
2019-04-21 01:29:20 +0000

    Refactor activations.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test 'Should not allow invalid signups' do
     get signup_path
     assert_no_difference 'User.count' do
@@ -15,7 +19,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors', 8
   end
   
-  test 'Should allow valid signups' do
+  test 'Should allow valid signups with account activation' do
     get signup_path
     assert_difference 'User.count' do
       post users_path, params: { user: { name: 'Example Guy',
@@ -23,8 +27,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password: 'password',
                                           password_confirmation: 'password' } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    assert_template 'static_pages/home'
-    assert_select 'div.alert-info'
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
