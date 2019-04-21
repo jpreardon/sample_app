a6091660e6713fdf2c876fc2b293d2ea66fd1f63
John P. Reardon <jp@jpreardon.com>
2019-04-21 00:01:45 +0000

    Activation email tests.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,8 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password_confirmation: 'password' } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert_select 'div.alert-success'
-    assert is_logged_in?
+    assert_template 'static_pages/home'
+    assert_select 'div.alert-info'
   end
 end
