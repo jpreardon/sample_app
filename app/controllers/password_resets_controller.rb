ca0767a5c655d716fe20a05e400a297d28410672
John P. Reardon <jp@jpreardon.com>
2019-04-22 22:17:57 +0000

    Password reset methods.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,9 @@
 class PasswordResetsController < ApplicationController
-  def new
-  end
+  before_action :get_user,          only: [:edit, :update]
+  before_action :valid_user,        only: [:edit, :update]
+  before_action :check_expiration,  only: [:edit, :update]
   
-  def edit
+  def new
   end
   
   def create
@@ -22,4 +23,42 @@ class PasswordResetsController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+  
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+    
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+    
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
 end
