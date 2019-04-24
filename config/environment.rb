34d26d37742b11053da1421bee79d18be2ba3db3
John P. Reardon <jp@jpreardon.com>
2019-04-24 02:32:04 +0000

    Add photo uploading.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# https://stackoverflow.com/questions/44004065/why-does-carrierwave-cause-the-nameerror-uninitialized-constant-micropostpict
+# require 'carrierwave/orm/activerecord'
